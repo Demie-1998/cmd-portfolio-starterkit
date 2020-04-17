24c9b76f4e7e3bdc65a2a2bc0fdfa4bc1db4e90f
Demie-1998 <60937320+Demie-1998@users.noreply.github.com>
2020-04-17 12:40:53 +0200

    Push1

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,6 +1,11 @@
 <?php snippet('header') ?>
-<img src="<?= $site->url() ?>/img/foto1.jpg">
-<main class="pt-5 pb-5">
+
+<main class="pt-3 pb-5">
+  <div class="row pb-4">
+  <div class="col-12">
+  <img src="<?= $site->url() ?>/img/foto1.jpg" class="align-left medium"/>
+</div>
+</div>
   <div class="row">
     <div class="col-12 col-lg-9"> 
       <?php snippet('intro') ?>
@@ -12,5 +17,6 @@
     </div>
   </div>
 </main>
+
 
 <?php snippet('footer') ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,39 +1,58 @@
 <?php snippet('header') ?>
 
 <main>
+  <div id="topcolor" >
   <div class="row align-items-center pt-5 pb-5">
     <div class="col-12 col-lg-6">
+      <div id="h1"> 
       <h1><?= $page->introductiontitle() ?></h1>
-      <p class="lead"><?= $page->introductiontext() ?></p>
+      </div>
+      <p id="introtekst" class="lead"><?= $page->introductiontext() ?></p>
       <a href="<?= $site->page('projects') ?>" class="btn btn-primary" role="button" aria-pressed="true">View my work</a>
       <a href="<?= $site->page('about') ?>" class="btn btn-outline-primary" role="button" aria-pressed="true">More about me</a>
     </div>
+    
+
     <div class="col-12 col-lg-6"><div class="w3-container">
 
       <img src="<?= $page->image()->url(); ?>" class="img-fluid" alt="<?= $page->image()->title(); ?>" />
     </div>
+</div>
   </div>
+</main>
+</div>
+
+<div id="pijl">
+    <img id="pijlimg" src="img/Punt3.png"> 
+    
+    </div>
+<div class="container">
+<main>
+ 
+
   <?php
   if ($projectsPage = page('projects')): ?>
-  <div class="row border-top pt-5 pb-5">
+  <div class="row pt-5 pb-5">
     <div class="col-12">
       <h2 class="float-left pb-4">My work</h2>
       <a class="float-right" href="<?= $site->page('projects') ?>">View all projects &#8594;</a>
     </div>
     <?php foreach ($projectsPage->children()->listed()->paginate(3) as $project): ?>
     <div class="col-12 col-lg-4 mb-4">
-      <div class="card h-100">
+      <div id="hovercard" class="card h-100">
       <img src="<?= $project->images()->findBy("template", "thumbnail")->url(); ?>" class="card-img-top" alt="thumbnail" />
-      <div class="card-body">
-        <h4 class="card-title"><a href="<?= $project->url() ?>"><?= $project->title() ?></a></h4>
-        <p class="card-text"><?= $project->roles() ?></p>
-        <a href="<?= $project->url() ?>">Read more &#8594;</a>
+      <div class="card-body"> 
+      <h4 class="card-title"><a href="<?= $project->url() ?>"><?= $project->title() ?></a></h4>
+          <p class="card-text"><?= $project->roles() ?></p>
+          <a href="<?= $project->url() ?>">Read more &#8594;</a>
       </div>
     </div>
     </div>
     <?php endforeach ?>
   </div>
   <?php endif ?>
+    </div>
 </main>
+
 
 <?php snippet('footer') ?>
